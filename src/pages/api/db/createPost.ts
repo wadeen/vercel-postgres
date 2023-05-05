b7e8f5a7f2e969be2042cd95343b55bef319c554
wadeen <meister.vvdo@gmail.com>
2023-05-05 19:34:41 +0900

    move db functions

--- src/pages/api/db/createPost.ts
+++ src/pages/api/db/createPost.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../../lib/prisma";
+import { prisma } from "../../../../lib/prisma";
 
 const CreatePost = async (request: NextApiRequest, response: NextApiResponse) => {
   const { title, description } = JSON.parse(request.body);
--- src/pages/api/db/deletePost.ts
+++ src/pages/api/db/deletePost.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../../lib/prisma";
+import { prisma } from "../../../../lib/prisma";
 
 const DeletePost = async (request: NextApiRequest, response: NextApiResponse) => {
   const { id } = JSON.parse(request.body);
